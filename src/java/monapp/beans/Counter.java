8bf509a90a766f520543b7f3d2159a4913000b58
Baptiste Domange <baptiste.domange@gmail.com>
2017-05-29 16:41:04 +0200

    3.2 done, now 3.3 : Portée des Managed Beans

---
+++ src/java/monapp/beans/Counter.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monapp.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class Counter {
+
+    Integer value = 1000;
+
+    public String inc() {
+        value++;
+        return null; // ne pas se déplacer
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
