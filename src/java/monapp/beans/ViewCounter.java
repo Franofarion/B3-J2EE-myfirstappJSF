209a64b9c8543cef6b0ee85fc25a70bc6d70b07d
Baptiste Domange <baptiste.domange@gmail.com>
2017-05-29 17:09:33 +0200

    3.4 done || now 4 : Une application complète

--- src/java/monapp/beans/ViewCounter.java
+++ src/java/monapp/beans/ViewCounter.java
@@ -8,13 +8,14 @@ package monapp.beans;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-
-@ManagedBean()
+import javax.inject.Named;
+        
+@Named(value="applicationCounter")
 @ViewScoped
 public class ViewCounter implements Serializable {
 
+    ApplicationCounter appCounter;
     private static final long serialVersionUID = 7983140976075649622L;
     int value = 0;
 
